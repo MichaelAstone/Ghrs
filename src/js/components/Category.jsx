1adb038f7a7a947c9763fdfb25f806ea6458259d
Ixax Tavarez <i.tavarez@umiami.edu>
2018-08-25 13:45:04 +0000

    starts rating finished, productcard finished, similritem completed, and category menu completed

---
+++ src/js/components/Category.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+
+
+class Dropdown extends React.Component {
+constructor(){
+ super();
+
+ this.state = {
+       displayMenu: false
+     };
+
+  this.showDropdownMenu = this.showDropdownMenu.bind(this);
+  this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
+
+}
+
+showDropdownMenu(event) {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+    document.addEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+  hideDropdownMenu() {
+    this.setState({ displayMenu: false }, () => {
+      document.removeEventListener('click', this.hideDropdownMenu);
+    });
+
+  }
+
+  render() {
+    return (
+        <div  className="dropdown" style = {{background:"white",width:"320px", border: "solid 1px", margin: "30px", textAlign: "center"}} >
+            <div className="button" onClick={this.showDropdownMenu}> Category </div>
+
+            { this.state.displayMenu ? (
+                <ul>
+                    <li><a className="active" href="">item 1</a></li>
+                    <li><a href="">item 2</a></li>
+                    <li><a href="">item 3</a></li>
+                   
+                </ul>
+        ):
+        (
+          null
+        )
+        }
+
+        </div>
+
+    );
+  }
+}
+
+export default Dropdown;
--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-
+import 'bootstrap';
 //include your index.scss file into the bundle
 import '../styles/index.scss';
 
 //import your own components
 import Layout from './Layout.jsx';
+import ProductCard from './components/ProductCard.jsx';
 
-//render your react application
+
 ReactDOM.render(
-    <Layout />,
+    <ProductCard />,
     document.querySelector('#app')
 );
