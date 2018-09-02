16d731d0d1f56b7c445b64f20ca1798386519bec
Victor Garcia <vicks1008@gmail.com>
2018-09-02 20:12:36 +0000

    just pulling

--- src/js/components/CarouselSlider.jsx
+++ src/js/components/CarouselSlider.jsx
@@ -5,12 +5,12 @@ import '../../styles/JumbotronStyles.scss';
 import CarouselImage1 from '../../img/asset39.png';
 import CarouselImage2 from '../../img/asset42.png';
 import CarouselImage3 from '../../img/asset42.png';
-import  {Carousel, CarouselCaption, CarouselInner, CarouselItem, View, Mask, Container } from 'mdbreact';
+import  {Animation, Carousel, CarouselCaption, CarouselInner, CarouselItem, View, Mask, Container } from 'mdbreact';
 
 class CarouselSlider extends React.Component {
   render(){
     return(
-        <Container>
+        <Container className="carousel-container">
             <h4 className="mt-5 mb-2">{this.props.h4titletext}</h4>
             <Carousel
               activeItem={1}
@@ -21,8 +21,10 @@ class CarouselSlider extends React.Component {
                 <CarouselInner>
                     <CarouselItem itemId="1">
                         <View>
-                            <img className="d-block w-100" src={this.props.ImgUrl1} alt="First slide" />
-                            <Mask overlay="black-light"></Mask>
+                            <Animation type="fadeInUp">
+                                <img className="d-block w-100" src={this.props.ImgUrl1} alt="First slide" />
+                                <Mask overlay="black-light"></Mask>
+                            </Animation>
                         </View>
                         <CarouselCaption>
                             <h3 className="h3-responsive">{this.props.h3title1}</h3>
