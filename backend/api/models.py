3aba65a4618b1e8e27bb00ec2b3fac5c2bfbcbbe
Victor Garcia <vicks1008@gmail.com>
2018-09-27 22:37:35 +0000

    Added Victor's models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,16 +1,37 @@
 from rest_framework import serializers
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here. 
-class User(models.Model):
-    first_name = models.CharField(max_length=20, default="")
-    last_name = models.CharField(max_length=20, default="")
-    email = models.EmailField(max_length=50, default="")
-    phone = models.CharField(max_length=50, default="")
-    address = models.CharField(max_length=50, default="")
-    
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'firstname','lastname','email', 'phone', 'address')
+
+class Special(models.Model):
+    expiration = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, blank=True)
+    percentage = models.DecimalField(
+        max_digits=2, decimal_places=1, default=0, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Coupon(models.Model):
+    expiration = models.DateTimeField(auto_now_add=True)
+    usage_count = models.IntegerField(default=0, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class Transaction(models.Model):
+    product = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True)
+    special = models.ForeignKey(
+        Special, on_delete=models.CASCADE, blank=True)
+    shoppingCart = models.ForeignKey(
+        ShoppingCart, on_delete=models.CASCADE, blank=True)
+    coupon = models.ForeignKey(
+        Coupon, on_delete=models.CASCADE, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
