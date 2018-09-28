96f9b092e1cfb17b55f2f4d86651ec9379d1f742
Victor Garcia <vicks1008@gmail.com>
2018-09-28 01:09:15 +0000

    Fixed User import on views.py & product url in urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from api import views
+
 urlpatterns = [
     path('user/<int:user_id>', views.UserView.as_view(), name='id-user'),
     path('user/', views.UserView.as_view(), name='all-users'),
-    path('product/', views.UserView.as_view(), name='all-users'),
+    path('product/', views.UserView.as_view(), name='all-products'),
 ]
