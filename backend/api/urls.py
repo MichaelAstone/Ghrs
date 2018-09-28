541d5333703310c2c1fbef3e8cdd90daaf7939bf
Ixax Tavarez <i.tavarez@umiami.edu>
2018-09-28 00:57:54 +0000

    ixax urls done shopping cart category rating image purchase transaction

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,13 @@ from api import views
 urlpatterns = [
     path('user/<int:user_id>', views.UserView.as_view(), name='id-user'),
     path('user/', views.UserView.as_view(), name='all-users'),
-    path('product/', views.UserView.as_view(), name='all-users'),
+    path('product/', views.UserView.as_view(), name='product'),
+    path('shoppingcart/', views.ShoppingCart.as_view(), name='shoppingcart'),
+    path('category/', views.CategoryView.as_view(), name='category'),
+    path('rating/', views.RatingView.as_view(), name='rating'),
+    path('image/', views.ImageView.as_view(), name='image'),
+    path('purchase/', views.PurchaseView.as_view(), name='purchase'),
+    path('transaction/', views.TransactionView.as_view(), name='transaction'),
+    
+    
 ]
