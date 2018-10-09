52a7f11d8d7190976f3bd554fa418f142ed32bb6
Florencia <florenciaviera4@gmail.com>
2018-10-09 18:18:18 +0000

    made changes to the search bar

--- src/js/components/navbar/NavbarComponent.js
+++ src/js/components/navbar/NavbarComponent.js
@@ -4,16 +4,23 @@ import GeneralLogo from '../../../img/general-logo-with-tag.png';
 class NavbarComponent extends React.Component {
     render(){
         return(
-            <nav className="navbar navbar-default" role="navigation">
-                <a className="navbar-brand" href="#"><img src={GeneralLogo} /></a>
-                <div className="navbar-toggler" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <button className="login">Log In</button>
-                    <button className="signup">Sign Up</button>
-                    <button className="shoppingCart"><i className="fas fa-shopping-cart fa-1x"></i></button>
-                </div>
-            </nav>
+            <div>
+                <nav className="navbar navbar-default" role="navigation">
+                    <a className="navbar-brand" href="#"><img src={GeneralLogo} /></a>
+                    <div className="menuButtons">
+                        <button className="login">Log In</button>
+                        <button className="signup">Sign Up</button>
+                        <button className="shoppingCart"><i className="fas fa-shopping-cart fa-1x"></i></button>
+                    </div>
+                </nav>
+                <form className="searchBarContainer">
+                    <input className="searchInput" type="text" placeholder="Search"/>  
+                </form>  
+            </div>    
             );
     }
 }
 export default NavbarComponent;
 
+
+//need to make new component for search bar bc it doesnt fit in the navbar
